db52b064949de22f50f443909e74a3af1b07b4bd
StefanoM98 <Utente@PC-STEFANO>
2023-09-25 15:26:03 +0200

    Sottrazione, divisione e moltiplicazioni testati

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -25,6 +25,45 @@ namespace CalculatorTest
             Assert.IsTrue(realOutput == sum, $"Il risultato atteso era {realOutput}, mentre invece abbiamo {sum}" );
         }
 
-        
+        [Test]
+
+        public void TestSubtractFloat()
+        {
+            float x = 6.4f;
+            float y = 2.4f;
+
+            float realOutput = 4.0f;
+            float subtract = calculator.Subtract(x, y);
+
+            Assert.AreEqual(realOutput, subtract);
+        }
+
+        [Test]
+
+        public void TestDivideFloat()
+        {
+            float x = 10.0f;
+            float y = 2.0f;
+
+            float realOutput = 5.0f;
+            float divided = calculator.Divide(x, y);
+
+            Assert.AreEqual(realOutput, divided);
+        }
+
+        [Test]
+
+        public void TestMultiplyFloat()
+        {
+            float x = 10.0f;
+            float y = 2.0f;
+
+            float realOutput = 20.0f;
+            float multiplied = calculator.Multiply(x, y);
+
+            Assert.AreEqual(realOutput, multiplied);
+        }
+
+
     }
 }
