8269dccce18810147067b68df20d7da6cb755817
StefanoM98 <Utente@PC-STEFANO>
2023-09-25 15:18:54 +0200

    Somma

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -1,16 +1,30 @@
+using net_calculator_tester;
+
 namespace CalculatorTest
 {
     public class Tests
     {
+        Calculator calculator;
+
         [SetUp]
         public void Setup()
         {
+            calculator = new Calculator();
         }
 
         [Test]
-        public void Test1()
+        
+        public void TestAddFloat()
         {
-            Assert.Pass();
+            float x = 2.4f;
+            float y = 5.4f;
+
+            float realOutput = 7.8f;
+            float sum = calculator.Add(x, y);
+
+            Assert.IsTrue(realOutput == sum, $"Il risultato atteso era {realOutput}, mentre invece abbiamo {sum}" );
         }
+
+        
     }
 }
---
+++ net-calculator-tester/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    public class Calculator
+    {
+        public float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public float Subtract (float num1, float num2)
+        {
+            return (num1 - num2);
+        }
+
+        public float Divide (float num1, float num2)
+        {
+            return num1 / num2;
+        }
+
+        public float Multiply (float num1, float num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
