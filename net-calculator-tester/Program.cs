41cc7f1d3b64762c1e313d818a3da9c0d525d0b3
StefanoM98 <Utente@PC-STEFANO>
2023-09-25 15:33:50 +0200

    Provate le funzioni in Program

--- net-calculator-tester/Program.cs
+++ net-calculator-tester/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Calcolatrice");
+
+            Console.Write("Inserisci due numeri e dato delle funzioni ti faro una somma, una sottrazione, una moltiplicazione e una divisione: ");
+            float num1 = float.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+            try
+            {
+                float sum = Calculator.Add( (float) num1, (float)num2);
+                float subtract = Calculator.Subtract((float)num1, (float)num2);
+                float divide = Calculator.Divide((float)num1, (float)num2);
+                float multiply = Calculator.Multiply((float)num1, (float)num2);
+
+                Console.WriteLine($"Ecco il risultato della prima funzione{sum}");
+                Console.WriteLine($"Ecco il risultato della seconda funzione{subtract}");
+                Console.WriteLine($"Ecco il risultato della terza funzione{divide}");
+                Console.WriteLine($"Ecco il risultato della quarta funzione{multiply}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
